554dcb4c10249620a4f5b6014874885444e983d0
j-ka11 <joshuakall@utexas.edu>
2020-10-21 21:35:22 -0500

    Hello world Wordpress plugin.

---
+++ buttermilk-sizing-app.php
@@ -0,0 +1,9 @@
+<?php
+/**
+* Plugin Name: Buttermilk
+* Description: This is the plugin that implements buttermilk's sizing app.
+* Author: Joshua Kall
+**/
+add_action('wp_body_open', function() {
+   echo '<h1>hello!!!</h1>';
+});
